7caa2bab74c1aec77c912eec9c6de33c0fe1ef0b
vuongthanhtuan <18521609@gm.uit.edu.vn>
2023-05-27 17:11:40 +0700

    refactors: Added a command in the main() function

--- bikeshare.py
+++ bikeshare.py
@@ -200,7 +200,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
+        # this command is added for the final project
 
 if __name__ == "__main__":
 	main()
